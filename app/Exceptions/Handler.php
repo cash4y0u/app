70f0031db5f8ea5840952c63a0538abc77227780
laaraes <ester.campos.diy@gmail.com>
2021-06-28 23:27:17 -0300

    commit para arrumar handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,26 +2,40 @@
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
     protected $dontReport = [
+        //
     ];
 
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
     protected $dontFlash = [
+        'current_password',
         'password',
         'password_confirmation',
     ];
 
-    public function report(Exception $exception)
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
     {
-        parent::report($exception);
-    }
-
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
+        $this->reportable(function (Throwable $e) {
+            //
+        });
     }
 }
